a10b6ba21dc8665788ff0e412d3cf2ae87bf4c4f
ObledoAnguloEliasBaltazar <l23170263@culiacan.tecnm.mx>
2025-12-01 19:54:28 -0700

    Add initial HTML structure for p5.js project

---
+++ sketch.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Proyecto p5.js</title>
+  <script src="https://cdn.jsdelivr.net/npm/p5@1.9.3/lib/p5.min.js"></script>
+</head>
+<body>
+  <script src="sketch.js"></script>
+</body>
+</html>
