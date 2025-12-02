a37b25e72261310ddbec9f15b11e6818bf6173e7
ObledoAnguloEliasBaltazar <l23170263@culiacan.tecnm.mx>
2025-12-01 20:02:44 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,43 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="utf-8" /> 
+// Variable global para el canvas
+let cnv; 
+
+function setup() {
+    cnv = createCanvas(600, 400); 
+    cnv.parent(document.body); 
+    noLoop(); 
+}
+
+function draw() {
+    background(135, 206, 235); 
+
+    fill(34, 139, 34);
+    rect(0, 300, 600, 100); 
+
+
+    fill(255, 255, 0)
+    noStroke(); // Sin borde para el sol
+    ellipse(70, 70, 80, 80); 
+    fill(255, 100, 0); 
+    rect(200, 200, 200, 150); 
     
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    fill(139, 69, 19); 
+    triangle(200, 200, 400, 200, 300, 100); 
     
-    <title>Dibujo Casa p5.js</title>
+    fill(173, 216, 230); 
+    rect(240, 240, 40, 40); 
     
-    <script src="https://cdn.jsdelivr.net/npm/p5@1.9.3/lib/p5.min.js"></script>
-</head>
-<body>
-    <script src="sketch.js"></script>
-</body>
-</html>
+    fill(100, 50, 0); 
+    rect(320, 260, 40, 90);
+    
+    fill(255); 
+    noStroke(); 
+    
+    ellipse(450, 80, 80, 50);
+    ellipse(480, 70, 70, 40);
+    ellipse(420, 70, 60, 40);
+    
+    ellipse(180, 150, 60, 40);
+    ellipse(200, 140, 50, 30);
+    ellipse(160, 140, 40, 30);
+    
+}
